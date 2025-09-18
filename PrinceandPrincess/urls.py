bbb772b6fc1e18ece9bcd4c42b4986e6e5f490a1
Phantom <ramonadedotun550@gmail.com>
2025-09-18 21:10:22 +0100

    Migrating changes

--- PrinceandPrincess/urls.py
+++ PrinceandPrincess/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),  # Admin is now default page
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/users/', include('users.urls')),
--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import CartItemViewSet
 
 router = DefaultRouter()
-router.register('items', CartItemViewSet)
+router.register('items', CartItemViewSet, basename='cart-items')  # <-- basename added
 
 urlpatterns = router.urls
